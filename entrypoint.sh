20a16e42631b03eac11f41de513f5b1094b0e451
Mike Tango Bravo <mtb@openvino.org>
2025-05-12 13:50:33 -0300

    added bootnode (whiskey.openvino.org)

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ exec reth node \
   --datadir /data \
   --authrpc.addr 0.0.0.0 \
   --authrpc.port 8551 \
-  --authrpc.jwtsecret /jwt/jwt.hex
+  --authrpc.jwtsecret /jwt/jwt.hex \
+  --bootnodes=enode://debd3b2a9c7cd01f749123bf7a212428a56d29d640bed8c20ebbff554e43b03540f4b8c31bfeaa6ded809759840c66d34b47aab2dc93b92b2f198a083f776f88@167.99.127.208:30303
